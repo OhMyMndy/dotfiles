975df29c13fc8d4ed855608928e06f55cae858a1
Mandy Schoep <mandyschoep@gmail.com>
2017-04-17 19:21:25 +0200

    Added git + vim to arch installer

--- installers/arch.sh
+++ installers/arch.sh
@@ -12,6 +12,9 @@ fi
 
 sudo pacman -Syu yaourt
 yaourt -Syu
+yaourt -S git
+yaourt -S tig
+yaourt -S vim
 
 
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
