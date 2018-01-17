3a3dbb51369d39a52103dace8576342d5ca1bd6c
Mandy Schoep <mandy.schoep@simbuka.com>
2018-01-17 13:13:35 +0100

    Resolution changed script Change screenlayout automatic based on hostname Restart polybar on change in display settings (xrandr)

--- .screenlayout/1-display-1400x900.sh
+++ .screenlayout/1-display-1400x900.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
 xrandr --output HDMI2 --mode 1440x900 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off --output DP1 --off
-
-sleep 20
-~/bin/restart-polybar
--- .screenlayout/work.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off
-
-sleep 20
-~/bin/restart-polybar
