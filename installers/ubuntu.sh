56a1aa8839693fe9c7d7924398c316c69fb9e80b
Mandy Schoep <mandyschoep@gmail.com>
2019-03-03 09:21:56 +0100

    Added more to ubuntu installer, added mount tank script

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -12,7 +12,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function install_deb_from_url() {
     url="$1"
-    tmp="$(tempfile)"
+    tmp="$(mktemp)"
     curl "$url" >> "$tmp"
     sudo dpkg -i "$tmp"
 }
@@ -24,28 +24,43 @@ sudo update-initramfs -u
 
 sudo apt update
 
-sudo apt install vim git xfce4-terminal byobu tmux iotop htop zsh kcalc digikam cifs-utils libdbusmenu-gtk* vlc nmap sysfsutils sysstat \
-                 network-manager-openvpn kate remmina xdotool gimp kmymoney konversation -y
+sudo apt install vim git xfce4-terminal byobu tmux iotop htop zsh kcalc digikam cifs-utils libdbusmenu-gtk* vlc mpv nmap sysfsutils sysstat \
+                 network-manager-openvpn kate remmina xdotool gimp kmymoney konversation hfsprogs qdirstat parallel cdck -y
 
 sudo apt install kronometer ktimer -y
-sudo apt install xsel xclip -y
-sudo apt install virt-manager virt-viewer -y
+sudo apt install xsel xclip samba kdenetwork-filesharing -y
+sudo apt install virt-manager virt-viewer openssh-server k3b calligra -y
+
+sudo apt install exfat-fuse exfat-utils -y
+
+
+
+# Tor browser
+sudo apt install torbrowser-launcher -y
+
+
+sudo chmod 4711 /usr/bin/wodim; sudo chmod 4711 /usr/bin/cdrdao
 
 # Realtek drivers for MacBook
-sudo apt install kate firmware-b43-installer -y
+sudo apt install firmware-b43-installer -y
 
 sudo apt upgrade -y
 sudo apt autoremove -y
 
 
-sudo chsh -s $(which zsh) mandy
+sudo chsh -s "$(which zsh)" mandy
 
 sudo snap install intellij-idea-community --classic
 sudo snap install sublime-text --classic
 
+# Etcher
+echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+sudo apt-get update
+sudo apt-get install balena-etcher-electron -y
 
 
-bash $DIR/oh-my-zsh.sh
+bash "$DIR/oh-my-zsh.sh"
 
 
 # Fix for snaps with ZSH
@@ -69,6 +84,34 @@ rm Release.key
 
 
 
+# Qt development
+sudo apt install kdevelop qt5-default build-essential mesa-common-dev libglu1-mesa-dev -y
+
+# Development
+sudo apt install shellcheck -y
+sudo apt install wkhtmltopdf php-cli php-xml php-mbstring php-curl php-zip php-pdo-sqlite php-intl -y
+
+
+# Docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker "$(whoami)"  
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Locale
 sudo locale-gen nl_BE
