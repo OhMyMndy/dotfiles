a02fbe935f93c3dbe61a42cdb098e20cd7cde3df
Mandy Schoep <mandyschoep@gmail.com>
2019-09-20 17:24:12 +0200

    Added parcellite config

--- link.sh
+++ link.sh
@@ -34,6 +34,7 @@ mkdir -p ~/.local/share/xfpanel-switch/
 cp -f $DIR/.local/share/xfpanel-switch/*  ~/.local/share/xfpanel-switch/
 
 link-file "$DIR" '.config/fontconfig'
+link-file "$DIR" '.config/parcellite'
 link-file "$DIR" ".config/i3"
 link-file "$DIR" ".config/rofi"
 link-file "$DIR" ".config/quicktile.cfg"
