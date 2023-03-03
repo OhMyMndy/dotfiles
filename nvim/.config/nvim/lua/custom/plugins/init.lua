de01785f2fdcf0dfeca5375f0641cdefd6b173f3
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2023-03-03 12:36:38 +0100

    Neotree

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -1,1 +1,14 @@
-return {}
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+return {
+    {
+  "nvim-neo-tree/neo-tree.nvim",
+    config = {
+        branch = "v2.x",
+    },
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+}
