f29a5ae59b749c4daf209df3d0e223518a35b309
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-06-03 09:23:17 +0000

    Neovim config

--- home-cli.nix
+++ home-cli.nix
@@ -31,8 +31,32 @@
     nixpkgs-fmt
 
     go
+
+    cargo
+
     nodejs
+
     python3
+
+    php82
+    php82Extensions.curl 
+    php82Extensions.imagick 
+    php82Extensions.opcache 
+    php82Extensions.redis 
+    php82Extensions.pdo_mysql 
+    php82Extensions.pdo 
+    php82Extensions.mysqlnd
+    php82Extensions.openssl 
+    php82Extensions.posix 
+    php82Extensions.sodium 
+    php82Extensions.sockets 
+    php82Extensions.zip 
+    php82Extensions.yaml 
+    php82Extensions.xdebug
+    php82Packages.composer
+      
+    
+
   ];
 
 
