55bcdcc4ffb8181ea84263666b9b17dce97c6e66
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-07-24 11:51:01 +0000

    gui

--- home-gui.nix
+++ home-gui.nix
@@ -5,104 +5,16 @@
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ibm-plex
 
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.caffeine
-    gnomeExtensions.clipboard-indicator
-    gnomeExtensions.space-bar
-    gnomeExtensions.sound-output-device-chooser
-    albert
   ];
 
-  home.file.".config/albert/albert.conf".source = ./.config/albert/albert.conf;
 
   # see https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/
   gtk = {
     enable = true;
     gtk3.extraConfig = {
       Settings = ''
-        gtk-application-prefer-dark-theme=1
+        gtk-application-prefer-dark-theme=0
       '';
-    };
-  };
-
-  dconf.settings = {
-    "org/gnome/desktop/wm/keybindings" = {
-      "switch-input-source" = [ ];
-      "switch-input-source-backward" = [ ];
-      "switch-to-workspace-1" = [ "<Super>1" ];
-      "switch-to-workspace-2" = [ "<Super>2" ];
-      "switch-to-workspace-3" = [ "<Super>3" ];
-      "switch-to-workspace-4" = [ "<Super>4" ];
-      "switch-to-workspace-5" = [ "<Super>5" ];
-      "switch-to-workspace-6" = [ "<Super>6" ];
-      "switch-to-workspace-7" = [ "<Super>7" ];
-      "switch-to-workspace-8" = [ "<Super>8" ];
-      "switch-to-workspace-9" = [ "<Super>9" ];
-      "switch-to-workspace-10" = [ "<Super>10" ];
-    };
-    "org/gnome/desktop/search-providers" = {
-      disabled = [
-        "org.gnome.Nautilus.desktop"
-      ];
-    };
-    "org/gnome/desktop/wm/preferences" = {
-      num-workspaces = "10";
-      button-layout = "appmenu:minimize,maximize,close";
-    };
-    "org/gnome/mutter" = {
-      edge-tiling = true;
-      dynamic-workspaces = false;
-    };
-    "org/gnome/desktop/calendar" = {
-      "show-weekdate" = true;
-    };
-    "org/gnome/desktop/interface" = {
-      "enable-hot-corners" = false;
-      "monospace-font-name" = "JetBrainsMonoNL Nerd Font Mono 10";
-      "font-name" = "IBM Plex Sans 11";
-      "document-font-name" = "IBM Plex Sans 11";
-      "titlebar-font" = "IBM Plex Sans Bold 11";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      "binding" = "<Super>space";
-      "command" = "albert toggle";
-      name = "Albert";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      "custom-keybindings" = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-    "org/gnome/shell" = {
-      "enabled-extensions" = [
-        "clipboard-indicator@tudmotu.com"
-        "dash-to-dock@micxgx.gmail.com"
-        "caffeine@patapon.info"
-        "space-bar@luchrioh"
-        "sound-output-device-chooser@kgshank.net"
-      ];
-      "favorite-apps" = [
-        "org.gnome.Nautilus.desktop"
-        "vivaldi-stable.desktop"
-        "microsoft-edge.desktop"
-        "org.gnome.Console.desktop"
-        "code.desktop"
-        "idea-ultimate.desktop"
-        "carla.desktop"
-        "org.rncbc.qjackctl.desktop"
-      ];
-    };
-    "org/gnome/shell/extensions/dash-to-dock" = {
-      "dash-max-icon-size" = 32;
-      "preferred-monitor" = -2;
-      "dock-position" = "BOTTOM";
-      "dock-fixed" = true;
-      "show-trash" = false;
-      "show-mounts" = false;
-      "hot-keys" = false;
-      "click-action" = "focus-minimize-or-appspread";
-      "disable-overview-on-startup" = true;
-      "custom-theme-shrink" = true;
     };
   };
 
