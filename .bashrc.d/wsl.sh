aa7831dd4096fad05a7f24899746aee0d9d07029
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-07-24 11:58:04 +0000

    firejail prompt

--- .bashrc.d/wsl.sh
+++ .bashrc.d/wsl.sh
@@ -1,2 +1,4 @@
 # shellcheck shell=bash
-if grep -qi Microsoft /proc/version; then PS1="[$WSL_DISTRO_NAME] ${PS1}"; fi
+if grep -qi Microsoft /proc/version; then 
+    PS1="[$WSL_DISTRO_NAME] ${PS1}";
+fi
