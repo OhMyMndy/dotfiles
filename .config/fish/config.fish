f4b160668df2f3ea4526a74e218c1e78f9a78c2d
Mandy Schoep <mandyschoep@gmail.com>
2024-08-29 13:55:37 +0200

    Update fish config

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-end
