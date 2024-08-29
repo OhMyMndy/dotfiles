b0e98c7d7c0b6c0321387c57094d684f5b5b4a6a
Mandy Schoep <mandyschoep@gmail.com>
2024-08-29 14:09:38 +0200

    Neovim config changes

---
+++ .config/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,56 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- return {
+--   {
+--     "AstroNvim/astrocore",
+--     ---@type AstroCoreOpts
+--     opts = {
+--       mappings = {
+--         x = {
+--           ["LeaderP"] = { [["_dP]], { desc = "Paste" } },
+--         },
+--         -- first key is the mode
+--         n = {
+--           ["<Leader>P"] = { [["_dP]], { desc = "Paste" } },
+--           -- second key is the lefthand side of the map
+--           -- mappings seen under group name "Buffer"
+--           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+--           ["<Leader>bD"] = {
+--             function()
+--               require("astroui.status").heirline.buffer_picker(
+--                 function(bufnr) require("astrocore.buffer").close(bufnr) end
+--               )
+--             end,
+--             desc = "Pick to close",
+--           },
+--           -- tables with the `name` key will be registered with which-key if it's installed
+--           -- this is useful for naming menus
+--           ["<Leader>b"] = { name = "Buffers" },
+--           -- quick save
+--           -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+--         },
+--         t = {
+--           -- setting a mapping to false will disable it
+--           -- ["<esc>"] = false,
+--         },
+--       },
+--     },
+--   },
+-- }
+
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+--
+-- vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
+--
+-- -- greatest remap ever
+-- vim.keymap.set("x", "<leader>P", [["_dP]], { desc = "Paste" })
+--
+-- -- next greatest remap ever : asbjornHaland
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank" })
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+--
+-- vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Cut" })
--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -17,22 +17,5 @@
 --   },
 -- }
 
--- SEE: https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- TODO: not working
+vim.opt.clipboard = ""
