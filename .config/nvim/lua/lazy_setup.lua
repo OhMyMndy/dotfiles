3223a84b6d1defcd11db244da3bb5180c27def75
Mandy <mandy@dev-3.tail970d5.ts.net>
2024-09-22 15:15:12 +0200

    fix: astrocore no dev mode

--- .config/nvim/lua/lazy_setup.lua
+++ .config/nvim/lua/lazy_setup.lua
@@ -11,7 +11,9 @@ require("lazy").setup({
       update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
     },
   },
-  { "AstroNvim/astrocore", dev = true },
+  {
+    "AstroNvim/astrocore", --[[ dev = true ]]
+  },
   { import = "community" },
   { import = "plugins" },
 } --[[@as LazySpec]], {
