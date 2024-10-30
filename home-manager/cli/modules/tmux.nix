e6b4258f418f4b123cf274f32380f852480e4fc0
Mandy <mandy@dev-3.tail970d5.ts.net>
2024-10-30 10:38:07 +0100

    feat: add tmux plugins

--- home-manager/cli/modules/tmux.nix
+++ home-manager/cli/modules/tmux.nix
@@ -6,7 +6,42 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
-    extraConfig = builtins.readFile ./../../../.tmux.conf;
+    customPaneNavigationAndResize = true;
+    # keyMode = "vi";
+    # terminal = "screen-256color";
+    mouse = true;
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.continuum
+      # {
+      #   plugin = tmuxPlugins.dracula;
+      #   extraConfig = ''
+      #     set -g @dracula-battery-label false
+      #     set -g @dracula-no-battery-label true
+      #     set -g @dracula-show-battery-status false
+      #
+      #     # available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, tmux-ram-usage, network, network-bandwidth, network-ping, ssh-session, attached-clients, network-vpn, weather, time, mpc, spotify-tui, krbtgt, playerctl, kubernetes-context, synchronize-panes
+      #     set -g @dracula-plugins "cpu-usage gpu-usage ram-usage synchronize-panes"
+      #   '';
+      # }
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -ogq @catppuccin_pane_status_enabled "yes" # set to "yes" to enable
+          # set -ogq @catppuccin_pane_active_border_style "##{?pane_in_mode,fg=#{@thm_lavender},##{?pane_synchronized,fg=#{@thm_mauve},fg=#{@thm_lavender}}}"
+        #   set -ogq @catppuccin_pane_border_status "yes" # set to "yes" to enable
+        '';
+      }
+      tmuxPlugins.resurrect
+      tmuxPlugins.prefix-highlight
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.yank
+      tmuxPlugins.pain-control
+    ];
+    # extraConfig = builtins.readFile ./../../../.tmux.conf;
+    extraConfig = ''
+      set -g pane-border-status top
+    '';
   };
 
 }
