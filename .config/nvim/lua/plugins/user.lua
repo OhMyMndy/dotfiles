7004ecb0360383f545754613eb6a0c5801d2e265
Mandy <mandy@dev-3.tail970d5.ts.net>
2024-10-30 14:09:58 +0100

    feat: neovim catppuccin

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -5,7 +5,7 @@
 
 ---@type LazySpec
 return {
-  { "Mofiqul/dracula.nvim" },
+  -- { "Mofiqul/dracula.nvim" },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
