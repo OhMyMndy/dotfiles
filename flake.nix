ad400179075792153426464e8ce2e783da5f5c99
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-01 14:38:24 +0100

    feat: set git user and email automatically

--- flake.nix
+++ flake.nix
@@ -11,6 +11,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    nixpkgs-staging.url = "github:nixos/nixpkgs/staging";
 
     home-manager = {
       url = "github:nix-community/home-manager/master"; # /release-24.05";
@@ -23,6 +25,8 @@
     {
       nixpkgs,
       nixpkgs-unstable,
+      nixpkgs-master,
+      nixpkgs-staging,
       home-manager,
       flake-utils,
       ...
@@ -39,9 +43,19 @@
           config.allowUnfree = true;
           system = system;
         };
+        master-pkgs = import nixpkgs-master {
+          config.allowUnfree = true;
+          system = system;
+        };
+        staging-pkgs = import nixpkgs-staging {
+          config.allowUnfree = true;
+          system = system;
+        };
         pkgs = stable-pkgs // {
           # provides alias for all unstable pkgs SEE: https://rexk.github.io/en/blog/nix-home-manager-flake-setup/
           unstable = unstable-pkgs;
+          master = master-pkgs;
+          staging = staging-pkgs;
           neovim = unstable-pkgs.neovim;
           neovim-unwrapped = unstable-pkgs.neovim-unwrapped;
         };
--- home-manager/cli/modules/git.nix
+++ home-manager/cli/modules/git.nix
@@ -30,6 +30,15 @@
     (cd "$HOME"
     touch ".gitconfig"
     ${pkgs.git}/bin/git config --global include.path ".gitconfig-delta")
+    
+    if ${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user &>/dev/null; then
+      user=$(${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | ${pkgs.jq}/bin/jq -r .name)
+      email=$(${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | ${pkgs.jq}/bin/jq -r ".[1].email")
+      echo "Setting $user <$email> as the default Git user..."
+
+      ${pkgs.git}/bin/git config --global user.name "$user"
+      ${pkgs.git}/bin/git config --global user.email "$email" 
+    fi
   '';
 
 }
