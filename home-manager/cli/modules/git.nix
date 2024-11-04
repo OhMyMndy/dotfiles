94e9bf09fd85cc84b506ee35c743df37c36efbff
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-04 09:50:09 +0100

    fix: show command to run to authenticate to github

--- home-manager/cli/modules/git.nix
+++ home-manager/cli/modules/git.nix
@@ -31,7 +31,8 @@
     touch ".gitconfig"
     ${pkgs.git}/bin/git config --global include.path ".gitconfig-delta")
     
-    if ${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user &>/dev/null; then
+    # TODO: add two if statements and add how to fix it
+    if ${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails >/dev/null; then
       user=$(${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | ${pkgs.jq}/bin/jq -r .name)
       email=$(${pkgs.gh}/bin/gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | ${pkgs.jq}/bin/jq -r ".[1].email")
       echo "Setting $user <$email> as the default Git user..."
