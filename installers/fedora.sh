f4aec363ca356ff14fe83a9827ca3c339ac38ec8
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-12 09:00:52 +0100

    feat: install firefox flatpak on Fedora

--- installers/fedora.sh
+++ installers/fedora.sh
@@ -6,9 +6,13 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 sudo dnf install -y git curl zsh podman-docker
+sudo dnf remove firefox -y
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.mozilla.firefox -y
 
 # TODO: only if we have gnome installed
-# sudo dnf install -y gnome-tweaks
-
+# sudo dnf install -y gnome-tweaks recoll
 
 ./tailscale.sh
+
