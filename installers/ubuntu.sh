6790d2c9d4eaf26f501545cd28a2cac25b6f4185
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-12 09:00:35 +0100

    fix: remove podman from ubuntu.sh

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$DIR" || exit 1
+
 sudo apt-get update
-sudo apt-get install -y git curl zsh podman-docker
+sudo apt-get install -y git curl zsh 
+
+snap remove firefox
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.mozilla.firefox -y
 
 # TODO: only if we have gnome installed
 # sudo apt-get install -y gnome-tweaks
 
 
 ./tailscale.sh
-
 
