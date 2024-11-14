03586d2d0cd30fb0de5d74e1da5a6c2e41f452ee
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:07:31 +0100

    feat: setup for helm with plugins, is disabled because of long build time

--- home-manager/cli/modules/kubernetes.nix
+++ home-manager/cli/modules/kubernetes.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
+let
+  myHelm = pkgs.wrapHelm pkgs.kubernetes-helm {
+    plugins = with pkgs.kubernetes-helmPlugins; [
+      helm-secrets
+      helm-diff
+      helm-s3
+      helm-git
+    ];
+  };
+
+  myHelmfile = pkgs.helmfile-wrapped.override { inherit (myHelm.passthru) pluginsDir; };
+in
 {
 
   home.packages = with pkgs; [
     cilium-cli
+    # myHelm
+    # myHelmfile
     karmor
-    kubernetes-helm
+    kubeconform
+    kustomize
     k3s # k3s, kubectl
     k9s
     talosctl
