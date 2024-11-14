8dd331f8b100ab2816005cff3ed761192b4dde52
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:08:01 +0100

    fix: flatpaks.nix formatting

--- home-manager/gui/modules/flatpaks.nix
+++ home-manager/gui/modules/flatpaks.nix
@@ -1,9 +1,6 @@
+{ lib, ... }:
 {
-  lib,
-  ...
-}:
-{
-   home.activation.setupFlathub = lib.hm.dag.entryAfter [ "installPackages" ] ''
+  home.activation.setupFlathub = lib.hm.dag.entryAfter [ "installPackages" ] ''
     /usr/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
   '';
 
