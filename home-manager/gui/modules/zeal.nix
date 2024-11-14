8e1c48e60157c93ef34a34a22507369e3436f375
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:18:06 +0100

    fix: zeal check if flatpak is installed

--- home-manager/gui/modules/zeal.nix
+++ home-manager/gui/modules/zeal.nix
@@ -1,18 +1,11 @@
+{ config, lib, ... }:
 {
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-{
-  home.packages = with pkgs; [
-
-  ];
 
   # Install zeal flatpak
   home.activation.setupZeal = lib.hm.dag.entryAfter [ "installPackages" ] ''
-    PATH="${config.home.path}/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:$PATH"
-    flatpak install flathub org.zealdocs.Zeal -y
+    if command -v /usr/bin/flatpak &>/dev/null; then
+      /usr/bin/flatpak install flathub org.zealdocs.Zeal -y
+    fi
   '';
 
 }
