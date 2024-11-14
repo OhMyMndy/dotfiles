1ddfe7f04cc3420ec3d44bd03dc3ec52ffc88f65
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:16:22 +0100

    fix: check if flatpak is installed before setting up flathub

--- home-manager/gui/modules/flatpaks.nix
+++ home-manager/gui/modules/flatpaks.nix
@@ -1,7 +1,9 @@
 { lib, ... }:
 {
   home.activation.setupFlathub = lib.hm.dag.entryAfter [ "installPackages" ] ''
-    /usr/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    if command -v /usr/bin/flatpak &>/dev/null; then
+      /usr/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    fi
   '';
 
 }
