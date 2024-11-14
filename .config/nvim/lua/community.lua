43d1c51b5ab289fc29b64a98e5499f7b5c0cdfa1
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:16:02 +0100

    feat: neovim plugins

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,11 +5,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  -- { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.debugging.telescope-dap-nvim" },
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+
   { import = "astrocommunity.editing-support.cloak-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-context" },
   {
@@ -20,9 +24,22 @@ return {
   },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   { import = "astrocommunity.git.blame-nvim" },
-  -- { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
 
   -- import/override with your plugins folder
 }
