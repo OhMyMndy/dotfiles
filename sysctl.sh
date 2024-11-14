0fa56310a69bea86c38216a9d6a5fab1ce3d40f3
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-14 13:08:45 +0100

    feat: remove contents from sysctl.sh which is now in tailscale.sh

--- sysctl.sh
+++ sysctl.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
 
-# Tailscale: https://tailscale.com/kb/1019/subnets
-echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
-
