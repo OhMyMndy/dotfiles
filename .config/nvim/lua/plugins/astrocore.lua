440d836905a9e04b78c8f5e79184fe14d012fc00
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-15 13:33:45 +0100

    feat: astronvim clipboard behaviour for x11, wayland and wsl

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -4,138 +4,119 @@
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
+--
+local under_wsl = vim.fn.has "wsl" == 1
+local clipboard = {}
+if under_wsl then
+  -- WSL clipboard integration using clip.exe and powershell.exe
+  clipboard = {
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = "powershell.exe Get-Clipboard -Raw",
+      ["*"] = "powershell.exe Get-Clipboard -Raw",
+    },
+    cache_enabled = false,
+  }
+else
+  -- Assume Linux; detect Wayland or X11
+  local session_type = os.getenv "XDG_SESSION_TYPE"
 
+  if session_type == "wayland" then
+    -- Using wl-clipboard for Wayland
+    clipboard = {
+      copy = {
+        ["+"] = "wl-copy",
+        ["*"] = "wl-copy",
+      },
+      paste = {
+        ["+"] = "wl-paste",
+        ["*"] = "wl-paste",
+      },
+      cache_enabled = true, -- Optional
+    }
+  elseif session_type == "x11" then
+    -- Using xclip for X11
+    clipboard = {
+      copy = {
+        ["+"] = "xclip -selection clipboard",
+        ["*"] = "xclip -selection primary",
+      },
+      paste = {
+        ["+"] = "xclip -selection clipboard -o",
+        ["*"] = "xclip -selection primary -o",
+      },
+      cache_enabled = true, -- Optional
+    }
+  end
+end
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
-  opts = function(_, opts)
-    -- Detect the operating system
-    local under_wsl = vim.fn.has "wsl" == 1
-    local clipboard = {}
-    if under_wsl then
-      -- WSL clipboard integration using clip.exe and powershell.exe
-      clipboard = {
-        copy = {
-          ["+"] = "clip.exe",
-          ["*"] = "clip.exe",
-        },
-        paste = {
-          ["+"] = "powershell.exe Get-Clipboard -Raw",
-          ["*"] = "powershell.exe Get-Clipboard -Raw",
-        },
-        cache_enabled = true,
-      }
-    else
-      -- Assume Linux; detect Wayland or X11
-      local session_type = os.getenv "XDG_SESSION_TYPE"
-
-      if session_type == "wayland" then
-        -- Using wl-clipboard for Wayland
-        clipboard = {
-          copy = {
-            ["+"] = "wl-copy",
-            ["*"] = "wl-copy",
-          },
-          paste = {
-            ["+"] = "wl-paste",
-            ["*"] = "wl-paste",
-          },
-          cache_enabled = true, -- Optional
-        }
-      elseif session_type == "x11" then
-        -- Using xclip for X11
-        clipboard = {
-          copy = {
-            ["+"] = "xclip -selection clipboard",
-            ["*"] = "xclip -selection primary",
-          },
-          paste = {
-            ["+"] = "xclip -selection clipboard -o",
-            ["*"] = "xclip -selection primary -o",
-          },
-          cache_enabled = true, -- Optional
-        }
-
-        -- Alternatively, to use xsel instead of xclip, uncomment the following:
-        -- vim.g.clipboard = {
-        --   copy = {
-        --     ['+'] = 'xsel --clipboard --input',
-        --     ['*'] = 'xsel --primary --input',
-        --   },
-        --   paste = {
-        --     ['+'] = 'xsel --clipboard --output',
-        --     ['*'] = 'xsel --primary --output',
-        --   },
-        --   cache_enabled = true,
-        -- }
-        -- else
-        --   -- Fallback or unsupported session type
-        --   vim.notify("Unsupported or unknown XDG_SESSION_TYPE: " .. tostring(session_type), vim.log.levels.WARN)
-      end
-    end
-    return require("astrocore").extend_tbl(opts, {
-      -- Configure core features of AstroNvim
-      features = {
-        large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-        autopairs = true, -- enable autopairs at start
-        cmp = true, -- enable completion at start
-        diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-        highlighturl = true, -- highlight URLs at start
-        notifications = true, -- enable notifications at start
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = false,
+      underline = true,
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        wrap = false, -- sets vim.opt.wrap
       },
-      -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-      diagnostics = {
-        virtual_text = false,
-        underline = true,
+      g = { -- vim.g.<key>
+        clipboard = clipboard,
+        -- configure global vim variables (vim.g)
+        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+        -- This can be found in the `lua/lazy_setup.lua` file
       },
-      -- vim options can be configured here
-      options = {
-        opt = { -- vim.opt.<key>
-          clipboard = clipboard,
-          relativenumber = true, -- sets vim.opt.relativenumber
-          number = true, -- sets vim.opt.number
-          spell = false, -- sets vim.opt.spell
-          signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-          wrap = false, -- sets vim.opt.wrap
-        },
-        g = { -- vim.g.<key>
-          -- configure global vim variables (vim.g)
-          -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-          -- This can be found in the `lua/lazy_setup.lua` file
-        },
-      },
-      -- Mappings can be configured through AstroCore as well.
-      -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-      mappings = {
-        -- first key is the mode
-        n = {
-          -- second key is the lefthand side of the map
+    },
+    -- Mappings can be configured through AstroCore as well.
+    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
 
-          -- navigate buffer tabs with `H` and `L`
-          L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-          H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        -- navigate buffer tabs with `H` and `L`
+        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
-          -- mappings seen under group name "Buffer"
-          ["<Leader>bD"] = {
-            function()
-              require("astroui.status.heirline").buffer_picker(
-                function(bufnr) require("astrocore.buffer").close(bufnr) end
-              )
-            end,
-            desc = "Pick to close",
-          },
-          -- tables with just a `desc` key will be registered with which-key if it's installed
-          -- this is useful for naming menus
-          ["<Leader>b"] = { desc = "Buffers" },
-          -- quick save
-          ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bD"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Pick to close",
         },
-        t = {
-          -- setting a mapping to false will disable it
-          -- ["<esc>"] = false,
-        },
+        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        ["<Leader>b"] = { desc = "Buffers" },
+        -- quick save
+        ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
       },
-    })
-  end,
+      t = {
+        -- setting a mapping to false will disable it
+        -- ["<esc>"] = false,
+      },
+    },
+  },
 }
