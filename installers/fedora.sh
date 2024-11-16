7b53932714e8a3a44692d123ff9c015d3400c4e0
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-16 23:39:41 +0100

    fix: extra packages for ubuntu and fedora

--- installers/fedora.sh
+++ installers/fedora.sh
@@ -5,11 +5,20 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
-sudo dnf install -y vim git curl zsh flatpak @development-tools
+# XDG utils is needed for gcloud for example
+sudo dnf install -y vim git curl zsh flatpak @development-tools \
+  unzip xdg-utils flatpak-xdg-utils python3-pip
 sudo dnf remove firefox -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub org.mozilla.firefox -y
+
+# dependencies for building Python
+sudo dnf install -y zlib-devel bzip2 bzip2-devel readline-devel \
+  sqlite-devel openssl-devel xz xz-devel libffi-devel tk-devel
+
+# build with asdf:
+# asdf install python $(asdf list all python | grep '^3.12' | tail -1)
 
 # TODO: only if we have gnome installed
 # sudo dnf install -y gnome-tweaks recoll
--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -6,7 +6,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 sudo apt-get update
-sudo apt-get install -y vim git curl zsh flatpak build-essential
+
+# XDG utils is needed for gcloud for example
+sudo apt-get install -y vim git curl zsh flatpak build-essential \
+  xdg-utils flatpak-xdg-utils unzip python3-pip python3-venv
 
 sudo snap remove firefox
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
