e45408adf11634204bc4f440eb61d3fafea8ac20
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-29 22:57:01 +0100

    fix: just say yes in install.sh to home-manager switch

--- install.sh
+++ install.sh
@@ -20,4 +20,4 @@ if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-time nix run .#just -- switch
+time yes | nix run .#just -- switch
