9fbd2a7ce870761009e4f083069f9b3e00a3cca6
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-11-29 22:53:01 +0100

    fix: install.sh

--- install.sh
+++ install.sh
@@ -6,31 +6,15 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 if ! command -v nix &>/dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm --extra-conf "trusted-users = $USER"
+ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm --extra-conf "trusted-users = $USER"
 fi
 
-
- sudo systemctl set-property nix-daemon.service CPUShares=$((50* $(nproc --all)))
+if [[ -d /run/systemd/system ]]; then
+ sudo systemctl set-property nix-daemon.service CPUShares=$((50 * $(nproc --all)))
  sudo systemctl set-property nix-daemon.service MemoryLimit=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo) / 2 / 1024))M
  sudo systemctl daemon-reload
  sudo systemctl restart nix-daemon
-
-
-## START FLOX
-CONF_FILE="/etc/nix/nix.conf"
-SUBSTITUTERS="extra-trusted-substituters = https://cache.flox.dev"
-PUBLIC_KEYS="extra-trusted-public-keys = flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-
-# Check if substituters are already in the config file
-if ! grep -Fxq "$SUBSTITUTERS" "$CONF_FILE"; then
-    echo "$SUBSTITUTERS" | sudo tee -a "$CONF_FILE" > /dev/null
 fi
-
-# Check if public keys are already in the config file
-if ! grep -Fxq "$PUBLIC_KEYS" "$CONF_FILE"; then
-    echo "$PUBLIC_KEYS" | sudo tee -a "$CONF_FILE" > /dev/null
-fi
-## END FLOX
 
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
