232267732794388de29b5f8f39059d0f4b81d57f
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-05 10:43:17 +0100

    feat: asdf nodejs specific version

--- home-manager/cli/modules/javascript.nix
+++ home-manager/cli/modules/javascript.nix
@@ -21,7 +21,10 @@
     PATH="${config.home.path}/bin:$PATH"
     . ~/.asdf/asdf.sh
     ${pkgs.asdf-vm}/bin/asdf plugin add nodejs
-    ${pkgs.asdf-vm}/bin/asdf install nodejs latest
-    ${pkgs.asdf-vm}/bin/asdf global nodejs latest
+    # ${pkgs.asdf-vm}/bin/asdf install nodejs latest
+    # ${pkgs.asdf-vm}/bin/asdf global nodejs latest
+
+    ${pkgs.asdf-vm}/bin/asdf install nodejs $(${pkgs.asdf-vm}/bin/asdf list all nodejs | grep '^22.' | tail -1)
+    ${pkgs.asdf-vm}/bin/asdf global nodejs $(${pkgs.asdf-vm}/bin/asdf list all nodejs | grep '^22.' | tail -1)
   '';
 }
