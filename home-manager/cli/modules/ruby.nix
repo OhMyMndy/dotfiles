a445561807423ca002d9726c3e661adc67ccaf87
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-13 16:03:24 +0100

    fix: ruby installation

--- home-manager/cli/modules/ruby.nix
+++ home-manager/cli/modules/ruby.nix
@@ -21,29 +21,16 @@
     # libossp_uuid # For uuid
   ];
 
-  # programs.rbenv = {
-  #   enable = false;
-  #   plugins = [
-  #     {
-  #       name = "ruby-build";
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "rbenv";
-  #         repo = "ruby-build";
-  #         rev = "v20240727";
-  #         hash = "sha256-OjTVSmJbf6ngRLS66+wLT8WscVwfYBxpXUP9X/RLbXs=";
-  #       };
-  #     }
-  #   ];
-  # };
-
   # TODO: install dependencies
   # sudo dnf install -y ruby ruby-devel autoconf gcc rust patch make bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel perl
   home.activation.setupRuby = lib.hm.dag.entryAfter ["installPackages"] ''
     PATH="${config.home.path}/bin:$PATH"
-    . ~/.asdf/asdf.sh
-    ${pkgs.asdf-vm}/bin/asdf plugin add ruby
-    RUBY_VERSION="$(${pkgs.asdf-vm}/bin/asdf list all ruby | grep -E '^3.3.[0-9]+$' | tail -1)"
-    ${pkgs.asdf-vm}/bin/asdf install ruby $RUBY_VERSION
-    ${pkgs.asdf-vm}/bin/asdf global ruby $RUBY_VERSION
+    # . ~/.asdf/asdf.sh
+
+    # TODO: check if dependencies are installed before attempting to install
+    # ${pkgs.asdf-vm}/bin/asdf plugin add ruby
+    # RUBY_VERSION="$(${pkgs.asdf-vm}/bin/asdf list all ruby | grep -E '^3.3.[0-9]+$' | tail -1)"
+    # ${pkgs.asdf-vm}/bin/asdf install ruby $RUBY_VERSION
+    # ${pkgs.asdf-vm}/bin/asdf global ruby $RUBY_VERSION
   '';
 }
