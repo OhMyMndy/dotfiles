d7092f77a987ca851cfa5b1bee086744f1a9dc9c
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-16 10:41:07 +0100

    fix: podman DOCKER_HOST should be set automatically by podman-docker

--- .bashrc.d/podman.sh
+++
@@ -1,3 +0,0 @@
-if [[ -f "$XDG_RUNTIME_DIR/podman/podman.sock" ]]; then
-  export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
-fi
--- .config/fish/conf.d/podman.fish
+++
@@ -1,3 +0,0 @@
-if test -f "$XDG_RUNTIME_DIR/podman/podman.sock"
-  set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-end
