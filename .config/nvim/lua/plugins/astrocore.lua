47199896e51a8d7879fb11ade9761e5f573f070a
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-18 23:35:58 +0100

    chore: remove <C-s> as quick save in neovim

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -112,7 +112,7 @@ return {
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
-        ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
       },
       t = {
         -- setting a mapping to false will disable it
