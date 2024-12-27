530902d5658f2ad3be5fefa8c4e1884f174294c9
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-27 14:46:26 +0100

    feat: add apt-file to ubuntu.sh

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -7,6 +7,8 @@ cd "$DIR" || exit 1
 
 sudo apt-get update
 
+sudo apt-get install apt-file -y
+
 # XDG utils is needed for gcloud for example
 sudo apt-get install -y vim git curl zsh flatpak build-essential \
   xdg-utils flatpak-xdg-utils unzip python3-pip python3-venv composer
