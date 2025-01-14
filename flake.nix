90afc0d63baef93dbdbcd9807d6bad2bb959d4c7
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-01-14 18:00:35 +0100

    feat: add minimal

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "OhMyMndy's Dotfiles!";
   nixConfig = {
-    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
@@ -14,24 +14,26 @@
     nixpkgs-staging.url = "github:nixos/nixpkgs/staging";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master"; #release-24.05";
+      url = "github:nix-community/home-manager/master"; # release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    nixpkgs-previous,
-    nixpkgs-unstable,
-    nixpkgs-master,
-    nixpkgs-staging,
-    home-manager,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-previous,
+      nixpkgs-unstable,
+      nixpkgs-master,
+      nixpkgs-staging,
+      home-manager,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         stable-pkgs = import nixpkgs {
           config.allowUnfree = true;
           inherit system;
@@ -53,37 +55,44 @@
           config.allowUnfree = true;
           inherit system;
         };
-        pkgs =
-          stable-pkgs
-          // {
-            # provides alias for all unstable pkgs SEE: https://rexk.github.io/en/blog/nix-home-manager-flake-setup/
-            previous = previous-pkgs;
-            unstable = unstable-pkgs;
-            master = master-pkgs;
-            staging = staging-pkgs;
-            # neovim = unstable-pkgs.neovim;
-            # neovim-unwrapped = unstable-pkgs.neovim-unwrapped;
-          };
-        le-just = pkgs.callPackage ./packages/just/default.nix {};
-      in {
+        pkgs = stable-pkgs // {
+          # provides alias for all unstable pkgs SEE: https://rexk.github.io/en/blog/nix-home-manager-flake-setup/
+          previous = previous-pkgs;
+          unstable = unstable-pkgs;
+          master = master-pkgs;
+          staging = staging-pkgs;
+          # neovim = unstable-pkgs.neovim;
+          # neovim-unwrapped = unstable-pkgs.neovim-unwrapped;
+        };
+        le-just = pkgs.callPackage ./packages/just/default.nix { };
+      in
+      {
         apps = {
-          home-manager = flake-utils.lib.mkApp {drv = pkgs.home-manager;};
-          just = flake-utils.lib.mkApp {drv = le-just;};
-          nixpkgs-fmt = flake-utils.lib.mkApp {drv = pkgs.nixpkgs-fmt;};
+          home-manager = flake-utils.lib.mkApp { drv = pkgs.home-manager; };
+          just = flake-utils.lib.mkApp { drv = le-just; };
+          nixpkgs-fmt = flake-utils.lib.mkApp { drv = pkgs.nixpkgs-fmt; };
         };
 
         packages = {
           homeConfigurations = {
+            "minimal" = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              modules = [ ./home-manager/minimal ];
+              extraSpecialArgs = {
+                inherit pkgs;
+              };
+            };
+
             "cli" = home-manager.lib.homeManagerConfiguration {
               inherit pkgs;
-              modules = [./home-manager/cli];
+              modules = [ ./home-manager/cli ];
               extraSpecialArgs = {
                 inherit pkgs;
               };
             };
             "gui" = home-manager.lib.homeManagerConfiguration {
               inherit pkgs;
-              modules = [./home-manager/gui];
+              modules = [ ./home-manager/gui ];
               extraSpecialArgs = {
                 inherit pkgs;
               };
--- justfile
+++ justfile
@@ -1,12 +1,10 @@
 set shell := ["bash", "-c"]
 
 switch:
-    if [[ "$GOOGLE_CLOUD_SHELL" == "true" ]]; then \
-        time nix run .#home-manager -- switch --flake .#minimal --impure -b backup \
-    else \
-        if [[ -n "$DISPLAY" ]]; then time nix run .#home-manager -- switch --flake .#gui --impure -b backup; fi \ 
-        if [[ -z "$DISPLAY" ]]; then time nix run .#home-manager -- switch --flake .#cli --impure -b backup; fi \
-    fi
+    if [[ "$GOOGLE_CLOUD_SHELL" == "true" ]]; then target=minimal; fi; \
+    if [[ -n "$DISPLAY" ]]; then target=gui; fi; \
+    if [[ -z "$DISPLAY" ]]; then target=gui; fi; \
+    if [[ -n "$target" ]]; then time nix run .#home-manager -- switch --flake .#"$target" --impure -b backup; fi;
 switch-gui:
     time nix run .#home-manager -- switch --flake .#gui --impure -b backup
 
