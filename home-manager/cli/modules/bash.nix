a421b3c2368bd46cf070c713ecb41889edf85a74
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-01-14 18:08:37 +0100

    fix: asdf install

--- home-manager/cli/modules/bash.nix
+++ home-manager/cli/modules/bash.nix
@@ -1,12 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.bash = {
     enable = true;
     bashrcExtra = ''
       ${builtins.readFile "${./../../../.bashrc}"}
-      . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
-      . "${pkgs.asdf-vm}/share/asdf-vm/completions/asdf.bash"
+      . "$HOME/.asdf/asdf.sh"
+      . "$HOME/.asdf/completions/asdf.bash"
     '';
-    # bashrcExtra = builtins.readFile "${./../../../.bashrc}";
   };
   home.file.".bashrc.d" = {
     source = ./../../../.bashrc.d;
