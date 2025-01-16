392e3464986fc1fd58fb873be1940361f0db2cf6
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-01-16 17:42:06 +0100

    feat: k3s setup with cert-manager, dashboard, reloader, cilium

--- installers/k3s.sh
+++ installers/k3s.sh
@@ -5,9 +5,20 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.30.8+k3s1 sh -s - --disable=servicelb --disable=traefik --disable-network-policy --flannel-backend=none --write-kubeconfig-mode 644
+if [[ ! -f /etc/rancher/k3s/k3s.yaml ]]; then
+  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.30.8+k3s1 sh -s - \
+    --disable-network-policy --flannel-backend=none --write-kubeconfig-mode 644
+  # --disable=servicelb --disable=traefik \
+fi
 
+sudo cp ../k3s/manifests/traefik-override.yaml /var/lib/rancher/k3s/server/manifests/traefik-override.yaml
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
 export KUBECONFIG
-helmfile -f ../k3s/helmfile.yaml sync
+
+IP_ADDRESS="$(ip route | awk '($1 == "default") {print $9}')"
+export IP_ADDRESS
+set -x
+helmfile -f ../k3s/helmfile.yaml sync --skip-refresh --color
