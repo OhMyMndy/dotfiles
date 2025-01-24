adfc0de97d7eb3c92fb0da48c19752155522e533
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-01-24 14:45:57 +0100

    fix: k3s install script for Fedora

--- installers/k3s.sh
+++ installers/k3s.sh
@@ -5,13 +5,28 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
-if [[ ! -f /etc/rancher/k3s/k3s.yaml ]]; then
-  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.30.8+k3s1 sh -s - \
-    --disable-network-policy --flannel-backend=none --write-kubeconfig-mode 644
-  # --disable=servicelb --disable=traefik \
+if command -v dnf &>/dev/null && ! command -v firewall-cmd &>/dev/null; then
+  sudo dnf install firewalld -y
+  sudo systemctl enable --now firewalld
 fi
 
-sudo cp ../k3s/manifests/traefik-override.yaml /var/lib/rancher/k3s/server/manifests/traefik-override.yaml
+if command -v dnf &>/dev/null && [[ ! -d /usr/share/bash_completion ]]; then
+  sudo dnf install bash-completion -y
+fi
+
+if [[ ! -f /etc/rancher/k3s/k3s.yaml ]]; then
+  sudo mkdir -p /etc/rancher/k3s
+  echo "nameserver 9.9.9.9" | sudo tee /etc/rancher/k3s/custom-resolv.conf >/dev/null
+  sudo mkdir -p /var/lib/rancher/k3s/server/manifests/
+  sudo cp ../k3s/manifests/traefik-override.yaml /var/lib/rancher/k3s/server/manifests/traefik-override.yaml
+
+  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.30.8+k3s1 sh -s - \
+    --write-kubeconfig-mode 644 \
+    --resolv-conf=/etc/rancher/k3s/custom-resolv.conf
+  #--disable=traefik
+  # ---flannel-backend=none  ---disable-network-policy -disable=servicelb \--disable-kube-proxy
+fi
+
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 sudo firewall-cmd --permanent --zone=public --add-service=http
@@ -20,5 +35,5 @@ export KUBECONFIG
 
 IP_ADDRESS="$(ip route | awk '($1 == "default") {print $9}')"
 export IP_ADDRESS
-set -x
-helmfile -f ../k3s/helmfile.yaml sync --skip-refresh --color
+
+helmfile -f ../k3s/helmfile.yaml sync --color
