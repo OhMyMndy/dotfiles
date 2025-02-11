265e6865cd92a2f8ee07ee8723ca40b66ba56e35
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-02-11 10:17:13 +0100

    fix: flatpak path not existing

--- home-manager/gui/modules/flatpaks.nix
+++ home-manager/gui/modules/flatpaks.nix
@@ -4,6 +4,7 @@
     # add path here so flatpak can access other binaries
     PATH="${config.home.path}/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:$PATH"
     if command -v /usr/bin/flatpak &>/dev/null; then
+      mkdir -p ~/.local/share/flatpak/repo/refs/remotes/ ~/.local/share/flatpak/repo/refs/heads/
       /usr/bin/flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     fi
   '';
