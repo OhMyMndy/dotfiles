81226708013448fb13aa094cd9eda6a8aadacb5f
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-02-14 10:54:51 +0100

    feat: tmux config

--- home-manager/cli/modules/tmux.nix
+++ home-manager/cli/modules/tmux.nix
@@ -17,6 +17,83 @@
     sensibleOnTop = true;
     baseIndex = 1;
     historyLimit = 10000;
+    plugins = with pkgs.master; [
+      # tmuxPlugins.cpu
+      # Since the version in nixpkgs is old, lets use it directly from GitHub
+      # {
+      #   plugin = tmuxPlugins.mkTmuxPlugin {
+      #     pluginName = "catppuccin";
+      #     version = "v2.1.0";
+      #     src = fetchFromGitHub {
+      #       owner = "catppuccin";
+      #       repo = "tmux";
+      #       rev = "d6458527ef121cc280c5dd119ba638749de1f713";
+      #       hash = "sha256-kWixGC3CJiFj+YXqHRMbeShC/Tl+1phhupYAIo9bivE=";
+      #     };
+      #   };
+      #   # plugin = tmuxPlugins.catppuccin;
+      #   extraConfig = ''
+      #     # set -g @catppuccin_flavor "mocha"
+      #     set -g @catppuccin_window_status_style "basic"
+      #     set -g status-right "#{E:@catppuccin_status_load}#{E:@catppuccin_status_session}#{E:@catppuccin_status_date_time}"
+      #     set -ag status-right "#[fg=#{@thm_fg},bg=#{@thm_mantle}] #(tmux-mem-cpu-load  -i 10 -g 0 -a 0 -m 0 -t 0) "
+      #     # set -ogq @catppuccin_pane_status_enabled "yes" # set to "yes" to enable
+      #     set -ogq @catppuccin_window_text " #{window_name}"
+      #     set -ogq @catppuccin_window_current_text " #{window_name}"
+      #     # # run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
+      #     # # set -g status-right ""
+      #     set -g status-right-length 100
+      #     # # set -g status-right "#{E:@catppuccin_status_application}"
+      #     # set -g status-right "#{prefix_highlight}"
+      #     # set -agF status-right "#{E:@catppuccin_status_cpu}"
+      #     # set -ag status-right "#{E:@catppuccin_status_session}"
+      #     # set -ag status-right "#{E:@catppuccin_status_uptime}"
+      #     # run ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+      #   '';
+      # }
+      {
+        # plugin = tmuxPlugins.resurrect;
+        plugin = tmuxPlugins.mkTmuxPlugin {
+          pluginName = "resurrect";
+          version = "v4.0.0";
+          src = fetchFromGitHub {
+            owner = "tmux-plugins";
+            repo = "tmux-resurrect";
+            rev = "e87d7d592cac97fa38c12395ebec042c154a1844";
+            hash = "sha256-44Ok7TbNfssMoBmOAqLLOj7oYRG3AQWrCuLzP8tA8Kg=";
+          };
+          postInstall = ''
+            rm -rf $target/run_tests
+            rm -rf $target/tests
+            rm -rf $target/lib/tmux-test
+          '';
+
+        };
+        extraConfig = ''
+          # set -g @resurrect-strategy-vim 'session'
+          # set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          # set -g @resurrect-processes ':all:'
+          set -g @resurrect-processes 'ssh lazygit less tail watch ssh "~nvim->nvim"'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '3'
+        '';
+      }
+      tmuxPlugins.prefix-highlight
+      {
+        plugin = tmuxPlugins.better-mouse-mode;
+        extraConfig = ''
+          set -g @emulate-scroll-for-no-mouse-alternate-buffer 'on'
+        '';
+      }
+      tmuxPlugins.yank
+      tmuxPlugins.pain-control
+    ];
     extraConfig = ''
       set -g pane-border-status top
       set -as terminal-features ",gnome*:RGB"
