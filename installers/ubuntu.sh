b770f396f05a4d59e687aee2973f05202b516901
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-02-22 16:38:43 +0100

    fix: ubuntu flathub

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -7,22 +7,24 @@ cd "$DIR" || exit 1
 
 sudo apt-get update
 
-sudo apt-get install apt-file moreutils -y
+sudo apt-get install apt-file moreutils sshfs inotify-tools -y
 
 # XDG utils is needed for gcloud for example
 sudo apt-get install -y vim git curl zsh flatpak build-essential \
-  xdg-utils flatpak-xdg-utils unzip python3-pip python3-venv
+	xdg-utils flatpak-xdg-utils unzip python3-pip python3-venv
 
 if [[ "$DISPLAY" != '' ]]; then
-  sudo snap remove firefox
-  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	sudo snap remove firefox
+	flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+	sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-  sudo flatpak install flathub org.mozilla.firefox -y
+	sudo flatpak install flathub org.mozilla.firefox -y
+	# flatpak install flathub io.gitlab.librewolf-community
 fi
 #
 # dependencies for building Python
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev curl git \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+	libbz2-dev libreadline-dev libsqlite3-dev curl git \
+	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 # TODO: only if we have gnome installed
 # sudo apt-get install -y gnome-tweaks
