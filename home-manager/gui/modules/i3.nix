d9b52c32cb4424d662db172f21687981dae7abff
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-03-26 14:18:21 +0100

    chore: disable i3

--- home-manager/gui/modules/i3.nix
+++ home-manager/gui/modules/i3.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
-  home.file.".config/i3" = {
-    source = "${config.home.homeDirectory}/dotfiles/.config/i3";
-    recursive = true;
-  };
+  # home.file.".config/i3" = {
+  #   source = "${config.home.homeDirectory}/dotfiles/.config/i3";
+  #   recursive = true;
+  # };
 }
