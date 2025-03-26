b69d6ed6134f7bda19927d8663313b306893b2af
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2025-03-26 14:19:33 +0100

    feat: install script add user to groups if they already exist

--- install.sh
+++ install.sh
@@ -7,19 +7,20 @@ cd "$DIR" || exit 1
 
 if ! command -v nix &>/dev/null; then
 
- if [[ ! -d /run/systemd/system ]]; then
-  sh <(curl -L https://nixos.org/nix/install) --no-daemon
+	if [[ ! -d /run/systemd/system ]]; then
+		sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
-  mkdir -p ~/.config/nix
-  echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
+		mkdir -p ~/.config/nix
+		echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
 
-  mkdir -p ~/.config/nixpkgs
-  echo "{ allowUnfree = true; }" >~/.config/nixpkgs/config.nix
- else
-  # Replace with lix
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
-   --no-confirm --extra-conf "trusted-users = $USER"
- fi
+		mkdir -p ~/.config/nixpkgs
+		echo "{ allowUnfree = true; }" >~/.config/nixpkgs/config.nix
+	else
+		# Replace with lix
+		curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+			--no-confirm --extra-conf "trusted-users = $USER"
+		nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+	fi
 
 fi
 
@@ -28,14 +29,15 @@ fi
 # fi
 
 if command -v vim &>/dev/null && [[ -d /etc/sudoers.d ]]; then
- echo "Defaults editor=$(command -v vim)" | sudo tee /etc/sudoers.d/editor
+	echo "Defaults editor=$(command -v vim)" | sudo tee /etc/sudoers.d/editor >/dev/null
 fi
 
-echo net.ipv4.ip_forward=1 | sudo tee /etc/sysctl.d/99-ip-forward.conf
+echo net.ipv4.ip_forward=1 | sudo tee /etc/sysctl.d/99-ip-forward.conf >/dev/null
 
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes.conf >/dev/null
 fs.inotify.max_user_watches = 524288
 fs.inotify.max_user_instances = 512
+net.ipv4.ip_forward=1 
 EOF
 # Uninstalling nix
 # sudo rm -rf /nix ~/.nix-*
@@ -43,21 +45,30 @@ EOF
 # cat /etc/group | grep -E '^nix' | cut -d':' -f1 | tr '\r\n' '\0' | sudo xargs -0 -I{} groupdel {}
 
 if [[ -d /run/systemd/system ]]; then
- echo "Tuning nix-daemon"
- sudo systemctl set-property nix-daemon.service CPUShares=$((50 * $(nproc --all)))
- sudo systemctl set-property nix-daemon.service MemoryLimit=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo) / 2 / 1024))M
- sudo systemctl daemon-reload
- sudo systemctl restart nix-daemon
+	echo "Tuning nix-daemon"
+	sudo systemctl set-property nix-daemon.service CPUShares=$((50 * $(nproc --all)))
+	sudo systemctl set-property nix-daemon.service MemoryLimit=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo) / 2 / 1024))M
+	sudo systemctl daemon-reload
+	sudo systemctl restart nix-daemon
 fi
 
 if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
- . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+	. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 elif [[ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
- . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+	. "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
 
-# echo "Running home manager"
-# time yes | nix run .#just -- switch
+function user_in_group() {
+	id -nG "$USER" | tr ' ' '\n' | grep -q "^$1$" && getent group "$1" &>/dev/null
+}
+
+groups_to_add=(docker libvirt libvirt-qemu kvm)
+
+for group in "${groups_to_add[@]}"; do
+  if ! user_in_group "$group"; then
+    sudo usermod -aG "$group" "$USER"
+  fi
+done
 
 # Bridge network
 # # TODO check if br0 already exists
