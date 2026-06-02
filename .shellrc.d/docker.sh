525debf10d2793cbfcaf693a9e8215f93b27168a
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2026-06-02 17:55:04 +0200

    fix: docker completion

--- .shellrc.d/docker.sh
+++ .shellrc.d/docker.sh
@@ -2,12 +2,6 @@
 
 if [ -n "$BASH" ] && command -v docker &>/dev/null; then
   . <(docker completion bash)
-  if docker compose version &>/dev/null; then
-    . <(docker compose completion bash)
-  fi
 elif command -v docker &>/dev/null; then
   . <(docker completion zsh)
-  if docker compose version &>/dev/null; then
-    . <(docker compose completion zsh)
-  fi
 fi
