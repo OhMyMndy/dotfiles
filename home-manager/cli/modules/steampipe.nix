3b8c20af6407bbaac6cc0181a6f5f513ed2aa130
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2026-07-08 17:45:00 +0200

    fix: steampipe jq call

--- home-manager/cli/modules/steampipe.nix
+++ home-manager/cli/modules/steampipe.nix
@@ -18,7 +18,7 @@
     plugins+="theapsgroup/keycloak turbot/kubernetes turbot/ldap turbot/linkedin turbot/net turbot/openapi turbot/prometheus turbot/steampipe "
     plugins+="turbot/terraform "
 
-    installed=$(steampipe plugin list --output json | jq '.installed[].name, .failed[].name' -r | sort | uniq | sed -E 's#(.*)/(.*)/(.*)@(.*)#\2/\3#g')
+    installed=$(steampipe plugin list --output json | jq '.installed[]?.name?, .failed[]?.name?' -r | sort | uniq | sed -E 's#(.*)/(.*)/(.*)@(.*)#\2/\3#g')
     new="$(echo "$plugins" | tr ' ' '\n' | tr '_' ' ')"
     to_install=$(comm -13 <(echo "$installed" | sort) <(echo "$new" | sort))
 
