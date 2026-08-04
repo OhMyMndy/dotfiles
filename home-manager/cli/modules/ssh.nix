019465ef3236c2d96f0983a467a499010bf61e2a
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2026-08-04 10:33:42 +0200

    feat: .ssh/config setup

--- home-manager/cli/modules/ssh.nix
+++ home-manager/cli/modules/ssh.nix
@@ -1,23 +1,8 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, dotfiles, ... }:
 {
   home.packages = with pkgs; [
   ];
 
-  home.activation.seupSSH = lib.hm.dag.entryAfter [ "installPackages" ] ''
-    mkdir -p ~/.ssh/
-    touch ~/.ssh/config
-
-    if ! grep -q "Include conf.d" ~/.ssh/config; then
-      cat <<EOF | tee -a ~/.ssh/config >/dev/null
-    Include conf.d/*
-    EOF
-    fi
-
-    if ! grep -q TERM= ~/.ssh/config; then
-      cat <<EOF | tee -a ~/.ssh/config > /dev/null
-    Host *
-      SetEnv TERM=xterm-256color
-    EOF
-    fi
-  '';
+  home.file.".ssh/config".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.ssh/config";
 }
